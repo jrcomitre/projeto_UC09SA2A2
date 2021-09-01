767a995236294d1523d4b86789f3066650e652eb
jrcomitre <gcomitrejr@gmail.com>
2021-09-01 11:20:16 -0300

    Primeiro Commit

---
+++ spec/PontosSpec.js
@@ -0,0 +1,11 @@
+describe("Teste de retorno da função sorteio()", function() {
+    it("Retorno deve ser menor do que 5", function() {
+        expect(sorteio()).toBeLessThan(5);
+    })
+})
+
+describe("Teste de retorno da função validar_pontos()", function() {
+    it("Retorno deve ser menor do que Vitória do Time 1, Vitória do Time, Empate", function() {
+        expect(validar_pontos()).(0, 0);
+    })
+})
